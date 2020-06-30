1d55437c0d5ef41779bcf9128fb65bdb9f3a1088
Ru Singh <hello@rusingh.com>
2020-06-30 12:35:05 +0530

    Delete other modules

--- modules/emoji-commit-helpers/.build-table.js
+++
@@ -1,5 +0,0 @@
-const emoji = require('./emoji');
-
-const output = `| Type | Emoji | Code | Description |\n| :--- | :---: | :--- | :--- |\n${emoji.map(e => `| ${e.name} | ${e.emoji} | \`${e.code}\` | ${e.description} |`).join('\n')}`;
-
-console.log(output);
